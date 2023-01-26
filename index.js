83953f9a6caacec2648c73ff2775935d6a407f15
ashlee.larrick <ashleemareelarrick323@gmail.com>
2023-01-26 10:23:09 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+function findMatching(names, filter_condition) {
+    return names.filter(function (name) {
+        return name.toLowerCase() == filter_condition.toLowerCase();
+      });
+}
+
+function fuzzyMatch(names, letters) {
+    return names.filter(
+            function (name) {
+        return name.startsWith(letters);
+        }
+      );
+}
+
+function matchName(drivers, list) {
+    return drivers.filter(
+        function (driver) {
+            return driver.name.toLowerCase() == list.toLowerCase();
+        }
+      );
+}
